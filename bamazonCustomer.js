bf09d06fa8ed372e7fa59e5bf6270eae188eedcc
FuzzyJones <donald.jones83@gmail.com>
2018-03-13 22:50:45 -0400

    working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
     if (err) throw err;
     // run functions below
     displayItems();
-    connection.end();
+    // connection.end();
   });
 
   function displayItems() {
@@ -25,22 +25,22 @@ var connection = mysql.createConnection({
         console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
       }
       console.log("----------------------------------------------------------");
-      runPurchase();
+      makePurchase();
     });
   }
 
-  function runPurchase() {
+function makePurchase() {
     inquirer
-      .prompt([
+    .prompt([
         {
             name: "item",
             type: "input",
-            message: "ID of item you would like to buy: ",
+            message: "ID # of item you would like to buy: ",
             validate: function(value) {
-              if (isNaN(value) === false) {
+            if (isNaN(value) === false) {
                 return true;
-              }
-              return false;
+            }
+            return false;
             }
         },
         {
@@ -48,14 +48,66 @@ var connection = mysql.createConnection({
             type: "input",
             message: "How many would you like to buy: ",
             validate: function(value) {
-              if (isNaN(value) === false) {
+            if (isNaN(value) === false) {
                 return true;
-              }
-              return false;
+            }
+            return false;
             }
         },
     ])
-      .then(function(answer) {
-        
-      });
-  }
+        .then(function(answer) {
+            connection.query("SELECT * FROM products WHERE item_id = ?", [answer.item], function(err, results) {
+            if (err) throw err;
+                // console.log(results);
+                // console.log(results[0]);
+                if (answer.amount > results[0].stock_quantity) {
+                    console.log("Sorry, not enough inventory to fill order");
+                    console.log("Quantity in stock = " + results[0].stock_quantity);
+                    newOrder();
+                } else {
+                    console.log("Thank you, your order has been placed");
+                    // rounding total price to 2 decimal places
+                    var num = results[0].price * answer.amount;
+                    var fixed_num = parseFloat(num).toFixed( 2 );
+                    console.log("Total Cost: $" + (fixed_num));
+                    // update inventory with amount purchased
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: results[0].stock_quantity - answer.amount
+                            },
+                            {
+                                item_id: answer.item
+                            }
+                        ],
+                        function(err, results) {
+                        
+                        }
+                    );
+                    newOrder();
+                }
+            })
+        });
+};
+
+// function to run a prompt asking if user wants to make another purchase
+// if yes, then run the make purchase function again
+// if no, then end connection
+function newOrder() {
+    inquirer
+    .prompt(
+      {
+        name: "choice",
+        type: "confirm",
+        message: "Would you like to make another purchase?"
+      },
+    ) .then(function(answer) {
+        if (answer.choice) {
+            makePurchase();
+        } else {
+            connection.end();
+        }
+    })
+};
+
+ 
